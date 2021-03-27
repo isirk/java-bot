be582324c60f444a684ff01e1a58b2e491829836
isirk <isirkin15@gmail.com>
2021-03-27 02:07:28 -0600

    added config

---
+++ src/main/kotlin/com/isirk/kotlinbot/ConfigDataClass.kt
@@ -0,0 +1,8 @@
+package com.isirk.kotlinbot
+
+data class ConfigDataClass (
+    val token: String,
+    val prefix: String,
+    val ownerId: String,
+    val serverInvite: String
+)
--- src/main/kotlin/com/isirk/kotlinbot/Main.kt
+++ src/main/kotlin/com/isirk/kotlinbot/Main.kt
@@ -1,24 +1,29 @@
 package com.isirk.kotlinbot
 
-import com.isirk.kotlinbot.modules.InfoModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.jagrosh.jdautilities.command.CommandClientBuilder
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.MemberCachePolicy
+import com.isirk.kotlinbot.modules.InfoModule
+import java.io.File
 import java.util.*
 
 fun main(){
 
+    val config: ConfigDataClass = jacksonObjectMapper().readValue<ConfigDataClass>(File("./config.json").readText())
+
     val builder: CommandClientBuilder = CommandClientBuilder()
-        .setPrefix("k!")
-        .setOwnerId("542405601255489537")
+        .setPrefix(config.prefix)
+        .setOwnerId(config.ownerId)
         .addAnnotatedModules(InfoModule())
         .useDefaultGame()
         .useHelpBuilder(true)
-        .setServerInvite("https://discord.gg/7yZqHfG")
+        .setServerInvite(config.serverInvite)
         .setEmojis("✅","⚠", "‼")
 
-    JDABuilder.createDefault("ODE5NjI3MDY4OTQ5MTM1NDAw.YEpXMQ.G_B162jtVUxcPYvC5_nkecVgYR8")
+    JDABuilder.createDefault(config.token)
         .addEventListeners(builder.build())
         .setEnabledIntents(EnumSet.allOf(GatewayIntent::class.java))
         .setMemberCachePolicy(MemberCachePolicy.ALL)
