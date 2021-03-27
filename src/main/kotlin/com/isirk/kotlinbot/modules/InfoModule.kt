35bda75c544ce7e8b9e5d99b92f34f641d119f3e
isirk <isirkin15@gmail.com>
2021-03-27 04:55:56 -0600

    Added Avatar Command and Choose Command (WIP)

--- src/main/kotlin/com/isirk/kotlinbot/modules/InfoModule.kt
+++ src/main/kotlin/com/isirk/kotlinbot/modules/InfoModule.kt
@@ -5,13 +5,14 @@ import com.jagrosh.jdautilities.command.annotation.JDACommand
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.User
 import com.isirk.kotlinbot.extensions.toMessage
 import com.isirk.kotlinbot.extensions.toNameDiscrim
 import com.isirk.kotlinbot.extensions.withInvisible
 import java.lang.Exception
 import java.time.format.DateTimeFormatter
 
-@JDACommand.Module(value = [ "ServerInfoCommand", "MemberInfoCommand" ])
+@JDACommand.Module(value = [ "ServerInfoCommand", "MemberInfoCommand", "ChooseCommand", "AvatarCommand" ])
 
 class InfoModule {
 
@@ -36,43 +37,67 @@ class InfoModule {
         }
     }
 
-    @JDACommand(
-        name = ["serverinfo", "guildinfo"],
-        help = "Sends info about the server"
-    )
+    @JDACommand(name = ["serverinfo", "server"], help = "Sends info about the server")
     fun ServerinfoCommand(event: CommandEvent){
         val guild: Guild = event.guild
 
         val builder = EmbedBuilder()
-            .setTitle("Info about `${guild.name}`")
+            .setTitle(guild.name)
             .setDescription(StringBuilder()
-                .append("Owner: ${guild.owner.toNameDiscrim()}").appendLine()
-                .append("ID: ${guild.id}").appendLine()
-                .append("Created At: ${guild.timeCreated.format(DateTimeFormatter.RFC_1123_DATE_TIME)}").appendLine()
-                .append("Verification Level: ${guild.verificationLevel.name.capitalize()}").appendLine()
-                .append("Region: ${guild.regionRaw.capitalize()}")
+                .append("**Owner:** ${guild.owner.toNameDiscrim()}").appendLine()
+                .append("**ID:** ${guild.id}").appendLine()
+                .append("**Created At:** ${guild.timeCreated.format(DateTimeFormatter.RFC_1123_DATE_TIME)}").appendLine()
+                .append("**Verification Level:** ${guild.verificationLevel.name.capitalize()}").appendLine()
+                .append("**Region:** ${guild.regionRaw.capitalize()}")
                 .toString())
             .setThumbnail(guild.iconUrl)
             .addField("Stats", StringBuilder()
-                .append("Member Count: ${guild.memberCount}").appendLine()
-                .append("Max Member Count: ${guild.maxMembers}").appendLine()
-                .append("Max Emotes: ${guild.maxEmotes}").appendLine()
-                .append("Max Filesize: ${guild.maxFileSize/bytesRatio} MB").appendLine()
-                .append("Emojis: ${guild.emotes.count()}")
+                .append("**Member Count:** ${guild.memberCount}").appendLine()
+                .append("**Max Member Count:** ${guild.maxMembers}").appendLine()
+                .append("**Max Emotes:** ${guild.maxEmotes}").appendLine()
+                .append("**Max Filesize:** ${guild.maxFileSize/bytesRatio} MB").appendLine()
+                .append("**Emojis:** ${guild.emotes.count()}")
                 .toString(),
                 false
+
             )
+            .withInvisible()
 
         event.reply(builder.toMessage())
     }
 
-    @JDACommand(
-        name = ["userinfo", "whois", "memberinfo"],
-        arguments = "member"
-    )
+    @JDACommand(name = ["user", "whois", "ui"], arguments = "user")
     fun MemberInfoCommand(event: CommandEvent){
-        val member: Member = resolveMember(event.args, event.guild)
+        if (event.args.isEmpty()) {
+            event.replyWarning("You need to supply a member.")
+        }
+        var member = resolveMember(event.args, event.guild)
             ?: return event.replyWarning("Member not found.")
+        val user = member.user
+        val builder = EmbedBuilder()
+            .setTitle("Info about `${member.toNameDiscrim()}`")
+            .setDescription(StringBuilder()
+                .append("Joined: ${member.timeJoined.format(DateTimeFormatter.RFC_1123_DATE_TIME)}").appendLine()
+                .append("ID: ${member.id}").appendLine()
+                .toString())
+            .setThumbnail(user.avatarUrl)
+            .withInvisible()
 
+        event.reply(builder.toMessage())
+    }
+
+    @JDACommand(name = ["avatar", "av"], help = "Get a users avatar")
+    fun AvatarCommand(event: CommandEvent){
+        event.reply(EmbedBuilder().setImage(event.author.avatarUrl).withInvisible().toMessage())
+    }
+
+    @JDACommand(name = ["choose"], arguments = "<item> <item> ...", help = "Choose a random item")
+    fun ChooseCommand(event: CommandEvent){
+        if (event.args.isEmpty()) {
+            event.replyWarning("You didn't give me any choices!")
+        } else {
+            var args = event.args.replace(" ", "")
+            event.reply("${args.toList()}")
+        }
     }
 }
